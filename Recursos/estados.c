4f2be87fdcb8825d1e883a11f9f979f864f65f2e
HSbruzzi <hugo.sbruzzi@gmail.com>
2023-12-06 12:27:38 -0300

    Revisado

--- Recursos/estados.c
+++ Recursos/estados.c
@@ -1,17 +1,19 @@
 
 #include "conf.h"
 
-
 estados_t f_espera(void){
    int nivel;
    nivel = leer_nivel();
    if(nivel <= (NIVEL_SET - NIVEL_OFFSET) ) {
+      RELE = 1; // Activa RELE en estado de carga
+      LED = 1;
       return CARGAR;
    }
-   else {   
-   return ESPERA;
+   else {
+      RELE = 0; // Apaga RELE en estado de espera
+      LED = 0;
+      return ESPERA;
    }
-
 }
 
 estados_t f_cargar(void){
@@ -19,12 +21,11 @@ estados_t f_cargar(void){
    nivel = leer_nivel();
    if(nivel >= (NIVEL_SET + NIVEL_OFFSET) ) {
        RELE = 0;
-       LED = 0;  
+       LED = 0;
       return ESPERA;
    } else {
-   RELE = 1;
-   LED = 1;
-   return CARGAR;
+      RELE = 1;
+      LED = 1;
+      return CARGAR;
    }
-   
 }
